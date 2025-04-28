9c40aa67d4193186a71afe732807918a12a963a1
Ubuntu <ubuntu@ip-172-31-3-160.ap-southeast-2.compute.internal>
2025-04-28 05:46:41 +0000

    Varibale Declaration and initialization Done

---
+++ Variables-in-shell.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# This is single line comments
+
+<< comment
+    this 
+    is
+    multiline coment
+comment
+
+
+#varibale Declaration and initialization
+
+name="Rohit" 
+
+echo "My name is $name"
+
+
+
+
