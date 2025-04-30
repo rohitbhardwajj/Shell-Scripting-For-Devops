941207c12d62bbb190bb7a5d9f6c689fdaa38b79
Ubuntu <ubuntu@ip-172-31-3-160.ap-southeast-2.compute.internal>
2025-04-30 07:04:00 +0000

    Done

--- Array.sh
+++ Array.sh
@@ -1,7 +1,24 @@
 #!/bin/bash
 
-#array
 
+
+
+## Array
 myArry=( 1 20 "rohit" "ram" )
+echo "${myArry[3]}"  # Output: ram
 
-echo "${myArry[3]}"
+## Length of array
+echo "Length of array is = ${#myArry[*]}"  # Output: 4
+
+## Extract specific part from an array
+echo "Value starting from index 1 (next 2 elements): ${myArry[*]:1:2}"  # Output: 20 rohit
+
+## Adding more element to array
+myArry+=("Mai hua naya add")
+echo "${myArry[*]}"  # Output: 1 20 rohit ram Mai hua naya add
+
+## Storing key-value pair in an associative array (IMPORTANT!)
+declare -A newArry  # 👈 Required for associative arrays
+newArry=( [name]="Rohit" [age]=23 )
+echo "${newArry[name]}"  # Output: Rohit
+
