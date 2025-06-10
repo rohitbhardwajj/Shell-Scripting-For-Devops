692d9ccdc2d090d444befdecc021867599fd0470
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-06-10 20:20:52 +0530

    Done

--- ReadFileContenet.sh
+++ ReadFileContenet.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 
+# using while loop
+
+
 while read var
 do 
     echo  "$var"
 
     done < file name or path
+
+
+
+    # using for loop
+
+
+    for i in  filename or path
+
+    do
+        echo "$i"
+        done
