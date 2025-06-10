e720f8458d72a8663e5eee1b474b1647e4f5f62f
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-06-10 19:59:29 +0530

    Done

--- WhileLoop.sh
+++ WhileLoop.sh
@@ -8,3 +8,6 @@ while [[ $i -le 10 ]]; do
     echo "$num * $i = $((num * i))"
         ((i++))
         done
+
+
+# one more loop in called until loop is same as while loop but it run until the condtion is false
