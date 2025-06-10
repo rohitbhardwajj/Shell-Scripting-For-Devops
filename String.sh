07e847b9d50ab9506cd8010cadad2ab8ba7a315f
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-06-10 11:28:33 +0530

    Done

---
+++ String.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#!/bin/bash
+
+name="Rohit"
+
+echo "${name}"
+
+
+#length of an string
+
+echo "${#name}"
+
+#upper case
+
+echo "${name^^}"
+
+#lower case
+
+echo "${name,,}"
+
+#replace
+
+new=${name/Rohit/mohit}
+echo "${new}"
+
+#slice
+
+afterSlice=${new:2:2}
+echo "${afterSlice}"
+~                                                                                                                                                                           ~                                                                                                                                                                           ~                                                                                                                                                                           ~                                                                                                                                                                           ~                                                                                                                                                                           ~                                                                                                                                                                           ~                                                                                                                                                                           ~                                   
