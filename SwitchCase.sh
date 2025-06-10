e8b7c4fe77b11c6178cbd74570f5c37ebf1e4e20
Rohit Bhardwaj <rohitbhardwaj121001@gmail.com>
2025-06-10 13:02:38 +0530

    Done

---
+++ SwitchCase.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter a number btween 1to 5"
+
+read choice
+
+case $choice in
+        1) echo "One";;
+        2) echo "Two";;
+        3) echo "Three";;
+        4) echo "Four";;
+        5) echo "Five";;
+        *) echo "Ivnvalid"
+esac
+~                                                                                                                                                                           ~                                                                                                                                                                           ~               
